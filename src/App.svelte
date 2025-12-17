89cddb1d54bd26a6e6010bb600bd01bbf1c28c49
tonymsp <antonio.cardona@microsip.com>
2025-12-17 17:34:18 -0600

    Update pricing PDF URL

--- src/App.svelte
+++ src/App.svelte
@@ -40,7 +40,7 @@
   {/if} -->
 
   <div class="link-precios">
-    <a href="https://cdn.prod.website-files.com/627983ccef617d453b9485ad/69433c64b51b2cc622b8e931_Microsip_Listas_de_precios_2025_interna_ajuste.pdf" target="_blank">Descargar lista de precios</a>
+    <a href="https://cdn.prod.website-files.com/627983ccef617d453b9485ad/69433dc499f8be8f3f818c4d_Microsip_Listas_de_precios_2025_UF_ajuste.pdf" target="_blank">Descargar lista de precios</a>
     <a href="https://www.microsip.com/promociones" target="_blank">Promociones</a>
   </div>
 
